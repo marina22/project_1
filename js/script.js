9cc16d75948a05386c6d46f69285e7f56c75cb17
Maryna <marina20471@gmail.com>
2024-03-15 12:04:17 +0300

    add text

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-"use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-number = 10;
-console.log(number);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
